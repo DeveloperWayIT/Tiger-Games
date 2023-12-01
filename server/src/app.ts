026a8d3810a57b5b72ac3cbf481b850e0b44340e
madnol <desole.manuel@gmail.com>
2023-12-01 10:07:08 +0100

    build(gg-14): build changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,14 +6,16 @@ import { userRoutes } from './routes/UserRoutes';
 
 const app = express();
 
-// Your existing server setup code
-console.log(process.env.NODE_ENV);
 // Serve static files from the React app in production
+const _dirname = path.dirname('');
+const buildPath = path.join(_dirname, '../client/dist');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
+  app.use(express.static(buildPath));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'), (err) =>
+      res.status(500).send(err)
+    );
   });
 }
 const port = process.env.PORT || 1337;
