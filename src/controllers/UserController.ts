99c2f3136c193e94ef779950dca1c13e178f2908
domenico-sarsano <147249166+domenico-sarsano@users.noreply.github.com>
2023-12-05 22:45:34 +0100

    other user methods

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from 'express';
 import { prisma } from '../utils/prisma';
+import { fillParametersData } from "../common/Common";
+import { parseBooleanDef } from "zod-to-json-schema";
+import { boolean } from "zod";
+
+interface UserCreateInput {
+  // All properties of the model:
+  UserId?: number;
+  Email: string;
+  Surname: string;
+  Name: string;
+  Password: string;
+  RoleId: number;
+  ActiveFlg: boolean;
+}
 
 export class UserController {
   static async getAllUsers(req: Request, res: Response) {
@@ -24,6 +38,70 @@ export class UserController {
     } catch (error) {
       console.error('Error retrieving User:', error);
       res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getUserByFilter(req: Request, res: Response) {
+    try {
+      const queryConditions: Record<string, any> = {};
+
+      fillParametersData(req.body, queryConditions);
+      const users = await prisma.role.findMany({
+        where: queryConditions,
+      });
+
+      res.json(users);
+    } catch (error) {
+      console.error("Error retrieving User by filter:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  static async createUser(req: Request, res: Response) {
+    try {
+      const data: UserCreateInput = { Email: "", Surname: "", Name: "", Password: "", RoleId: -1, ActiveFlg: false };
+      fillParametersData(req.body, data);
+
+      const result = await prisma.user.create({
+        data: data,
+      });
+
+      res.json(result);
+    } catch (error) {
+      console.error("Error creating User:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  static async deleteUser(req: Request, res: Response) {
+    try {
+      const data: Record<string, any> = {};
+
+      fillParametersData(req.body, data);
+      const result = await prisma.user.deleteMany({
+        where: data,
+      });
+
+      res.json(result);
+    } catch (error) {
+      console.error("Error deleting User:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+
+  static async updateUser(req: Request, res: Response) {
+    try {
+      const { conditions, updateData } = req.body;
+
+      const result = await prisma.user.updateMany({
+        where: conditions,
+        data: updateData,
+      });
+
+      res.json(result);
+    } catch (error) {
+      console.error("Error updating User:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
   }
 }
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -6,4 +6,9 @@ const userRoutes = Router();
 userRoutes.get("/User/all", UserController.getAllUsers);
 userRoutes.get("/User/:id", UserController.getUserById);
 
+userRoutes.post("/User/query", UserController.getUserByFilter);
+userRoutes.post("/User/create", UserController.createUser);
+userRoutes.post("/User/update", UserController.updateUser);
+userRoutes.post("/User/delete", UserController.deleteUser);
+
 export { userRoutes };
