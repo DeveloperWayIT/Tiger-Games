9a373161b941775154f802d465f62a695098edd0
Manuel Desole <46155957+madnol@users.noreply.github.com>
2023-12-16 18:21:33 +0100

    Delete tiger-games directory

--- tiger-games/src/store/counter/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '../index.types';
-import { CounterState } from './counter.types';
-
-// Selectors
-export const selectCounterState = (state: RootState): CounterState =>
-  state.counter;
